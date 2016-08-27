7292bf22d884fd5bb82f82e405ebff3efe921138
Sean Jensen-Grey <seanj@xyke.com>
2016-08-27 13:19:15 -0500

    requestAnimationFrame

--- docs/starting/canvas.lua
+++ docs/starting/canvas.lua
@@ -1,7 +1,23 @@
 canvas = window.document:getElementById("canvas")
 ctx = canvas:getContext("2d")
 
-ctx.fillStyle = "black"
-ctx:fillRect(0,0,512,512)
-ctx.fillStyle = "red"
-ctx:fillRect(128,128,256,256)
+draw_red_square = function(rotation)
+    ctx.fillStyle = "black"
+    ctx:fillRect(0,0,512,512)
+    ctx:save()
+        ctx:translate(256,256)
+        ctx:rotate(rotation)
+        ctx.fillStyle = "red"
+        ctx:fillRect(-64,-64,128,128)
+    ctx:restore()
+end
+
+rotation = 0.0
+
+update = function()
+    rotation = rotation + 0.005
+    draw_red_square(rotation)
+    window:requestAnimationFrame(update)
+end
+
+window:requestAnimationFrame(update)
